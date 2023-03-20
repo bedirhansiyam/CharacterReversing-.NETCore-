a5df315747fca73a2f2d3b4ba3d0ae303333313c
Bedirhan <bedirhansiyam@gmail.com>
2023-03-20 19:26:03 +0400

    First commit

---
+++ Program.cs
@@ -0,0 +1,15 @@
+﻿namespace CharacterReversing;
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.Write("Please enter a word : ");
+
+        string word = Console.ReadLine();
+        char firstLetter = word[0];
+        string newWord = word.Remove(0,1);
+        newWord = newWord + firstLetter;
+
+        Console.WriteLine(newWord);
+    }
+}
---
+++ README.md
@@ -0,0 +1,10 @@
+# Character Reversing - .Net Core
+In this application, the user enters a word and the application adds the first character to the end of the word and prints it on the console.
+</br>
+
+*Sample input: Hello - Welcome - Mauritius* </br>
+*Sample output: elloH - elcomeW - auritiusM*
+
+</br>
+
+[Patika.dev](https://app.patika.dev/)
